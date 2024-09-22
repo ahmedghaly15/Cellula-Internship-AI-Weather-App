27bb5b182ee48b4010a7244b877f82aea366e47a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-23 00:29:00 +0300

    Setuped firebase for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:internship_ai_weather_app/src/ai_weather_app.dart';
+import 'package:internship_ai_weather_app/src/core/firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // To fix texts being hidden bug in release mode
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AIWeatherApp());
 }
