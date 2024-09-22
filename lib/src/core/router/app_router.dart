f164f7d669297811179fa65422959e4d7c32dce0
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-22 23:28:22 +0300

    Initialized StartView & Updated AppRouter

--- lib/src/core/router/app_router.dart
+++ lib/src/core/router/app_router.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:internship_ai_weather_app/src/core/router/routes.dart';
+import 'package:internship_ai_weather_app/src/features/start/presentation/views/start_view.dart';
 
 class AppRouter {
   AppRouter._();
 
   static Route<dynamic> onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
+      case Routes.startRoute:
+        return MaterialPageRoute(
+          builder: (_) => const StartView(),
+        );
+
       default:
         return _unFoundRoute();
     }
---
+++ lib/src/features/start/presentation/views/start_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StartView extends StatelessWidget {
+  const StartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
