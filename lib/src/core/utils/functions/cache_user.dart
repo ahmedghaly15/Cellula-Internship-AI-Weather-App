612abc7b147186f453ee61bd2f465166f4951e7c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-23 16:57:28 +0300

    Built checkIfUserIsLoggedIn() method  - Built getFirebaseUserData() method  - Built cacheAppUser() method

--- lib/src/core/utils/functions/cache_user.dart
+++ lib/src/core/utils/functions/cache_user.dart
@@ -1,6 +1,16 @@
+import 'dart:convert';
+
 import 'package:internship_ai_weather_app/src/core/helpers/cache_keys.dart';
 import 'package:internship_ai_weather_app/src/core/helpers/secure_storage_helper.dart';
+import 'package:internship_ai_weather_app/src/core/models/app_user.dart';
 
 Future<void> cacheUserId(String userId) async {
   await SecureStorageHelper.setSecuredString(CacheKeys.userId, userId);
+}
+
+Future<void> cacheAppUser(AppUser appUser) async {
+  await SecureStorageHelper.setSecuredString(
+    CacheKeys.appUser,
+    json.encode(appUser.toJson()),
+  );
 }
