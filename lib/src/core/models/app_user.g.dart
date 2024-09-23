73014dc41165db696226d9c629e76d02db077300
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-23 16:10:36 +0300

    Built AppUser model

---
+++ lib/src/core/models/app_user.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AppUserImpl _$$AppUserImplFromJson(Map<String, dynamic> json) =>
+    _$AppUserImpl(
+      userId: json['userId'] as String,
+      email: json['email'] as String,
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$$AppUserImplToJson(_$AppUserImpl instance) =>
+    <String, dynamic>{
+      'userId': instance.userId,
+      'email': instance.email,
+      'name': instance.name,
+    };
