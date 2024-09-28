ebb1df2e1c8285bf4b57cf07911e38ee2ca5225b
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-28 16:58:18 +0300

    Built executeAndHandleErrors method

---
+++ lib/src/core/utils/functions/execute_and_handle_errors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:internship_ai_weather_app/src/core/api/api_error_handler.dart';
+import 'package:internship_ai_weather_app/src/core/api/api_result.dart';
+
+/// Executes a given asynchronous function and handles any errors that occur during its execution.
+///
+/// This method takes a [function] that returns a [Future] and attempts to execute it.
+/// If the function completes successfully, the result is wrapped in an [ApiResult.success] object and returned.
+/// If an error occurs during execution, the error is caught, logged using [debugPrint], and
+/// an [ApiResult.error] object is returned with the error handled by [ApiErrorHandler].
+///
+/// Example usage:
+/// ```dart
+/// Future<ApiResult<MyData>> result = executeAndHandleErrors(() async {
+///   // Your asynchronous code here
+/// });
+/// ```
+///
+/// [T] is the type of the result that the function returns.
+Future<ApiResult<T>> executeAndHandleErrors<T>(
+  Future<T> Function() function,
+) async {
+  try {
+    return ApiResult<T>.success(await function());
+  } catch (error) {
+    debugPrint('********* Error in executeAndHandleErrors: $error **********');
+    return ApiResult.failure(ApiErrorHandler.handle(error));
+  }
+}
