aa9a8e509a6a4752cd267d28421f0cddc273bc62
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-29 13:38:26 +0300

    Built ForecastApiService & Added fetchForecast() method to it

---
+++ lib/src/features/forecast/data/api/forecast_api_service.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:internship_ai_weather_app/src/core/api/end_points.dart';
+import 'package:internship_ai_weather_app/src/core/utils/app_keys.dart';
+import 'package:internship_ai_weather_app/src/features/forecast/data/models/fetch_forecast_response.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'forecast_api_service.g.dart';
+
+@RestApi(baseUrl: EndPoints.baseUrl)
+abstract class ForecastApiService {
+  factory ForecastApiService(Dio dio, {String baseUrl}) = _ForecastApiService;
+
+  @GET(EndPoints.fetchForecast)
+  Future<FetchForecastResponse> fetchForecast({
+    @Query('q') required String city,
+    @Query('key') String apiKey = AppKeys.apiKey,
+    @CancelRequest() CancelToken? cancelToken,
+  });
+}
