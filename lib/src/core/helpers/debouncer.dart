78ececb5a01c9a838a8fd49e13185046262cc813
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-29 03:09:51 +0300

    Built Debouncer

---
+++ lib/src/core/helpers/debouncer.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class Debouncer {
+  final Duration duration;
+  Timer? _timer;
+
+  Debouncer({required this.duration});
+
+  void run(VoidCallback action) {
+    _timer?.cancel();
+    _timer = Timer(duration, action);
+  }
+}
