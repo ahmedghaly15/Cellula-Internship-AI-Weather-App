3ba71a81d74597257a17aeac945f386820d3b49c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-30 06:50:59 +0300

    Finished current weather screen

--- lib/src/features/forecast/presentation/views/current_city_weather_view.dart
+++ lib/src/features/forecast/presentation/views/current_city_weather_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:internship_ai_weather_app/src/features/forecast/data/models/fetch_forecast_response.dart';
+import 'package:internship_ai_weather_app/src/features/forecast/data/models/labeled_circular_percent_attributes.dart';
+import 'package:internship_ai_weather_app/src/features/forecast/presentation/widgets/condition_text.dart';
+import 'package:internship_ai_weather_app/src/features/forecast/presentation/widgets/labeled_circular_percent_indicators.dart';
+import 'package:internship_ai_weather_app/src/features/forecast/presentation/widgets/weather_image.dart';
 
 class CurrentCityWeatherView extends StatelessWidget {
   const CurrentCityWeatherView({super.key, required this.currentCityWeather});
@@ -8,6 +13,51 @@ class CurrentCityWeatherView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            const SliverAppBar(title: Text('Current Weather')),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 32.h),
+                child: ConditionText(
+                  condition: currentCityWeather.condition!.text!,
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: LabeledCircularPercentIndicators(
+                attributes: _currentIndicatorsAttributes,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: WeatherImage(
+                imageUrl: currentCityWeather.condition!.icon!,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  List<LabeledCircularPercentAttributes> get _currentIndicatorsAttributes => [
+        LabeledCircularPercentAttributes(
+          label: 'Temp (°C)',
+          percent: currentCityWeather.tempC! / 100,
+        ),
+        LabeledCircularPercentAttributes(
+          label: 'Wind (km/h)',
+          percent: currentCityWeather.windKph! / 10,
+        ),
+        LabeledCircularPercentAttributes(
+          label: 'Humidity (%)',
+          percent: currentCityWeather.humidity! / 100,
+        ),
+        LabeledCircularPercentAttributes(
+          label: 'Cloud (%)',
+          percent: currentCityWeather.cloud! / 10,
+        ),
+      ];
 }
