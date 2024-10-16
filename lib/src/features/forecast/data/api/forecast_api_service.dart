4c16ecb11f2f47ea5021b7e50c2993c7946091d0
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-16 11:45:07 +0300

    Added tennisPlayPrediction() method to ForecasApiService

--- lib/src/features/forecast/data/api/forecast_api_service.dart
+++ lib/src/features/forecast/data/api/forecast_api_service.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:internship_ai_weather_app/src/core/api/end_points.dart';
 import 'package:internship_ai_weather_app/src/core/utils/app_keys.dart';
 import 'package:internship_ai_weather_app/src/features/forecast/data/models/fetch_forecast_response.dart';
+import 'package:internship_ai_weather_app/src/features/forecast/data/models/tennis_play_prediction_request.dart';
+import 'package:internship_ai_weather_app/src/features/forecast/data/models/tennis_play_prediction_response.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'forecast_api_service.g.dart';
@@ -17,4 +19,10 @@ abstract class ForecastApiService {
     @Query('key') String apiKey = AppKeys.apiKey,
     @CancelRequest() CancelToken? cancelToken,
   });
+
+  @POST(EndPoints.tennisPlayPrediction)
+  Future<TennisPlayPredictionResponse> tennisPlayPrediction(
+    @Body() TennisPlayPredictionRequest features, [
+    @CancelRequest() CancelToken? cancelToken,
+  ]);
 }
